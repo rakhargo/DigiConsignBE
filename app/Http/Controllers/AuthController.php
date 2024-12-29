7276bf93c952ed7c111f7657041161dc597cbae0
FRQNC <alhaqqi123@gmail.com>
2024-12-29 20:42:52 +0700

    dari zip haris

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user,
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'User registered successfully'], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\LokasiController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\KomentarController;
+use App\Http\Controllers\AkunController;
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -19,3 +22,8 @@ Route::apiResource('kategori', KategoriController::class);
 Route::get('product/{id}/komentar', [KomentarController::class, 'show']); // Mendapatkan komentar untuk produk tertentu
 Route::post('product/{id}/komentar', [KomentarController::class, 'storeComment']); // Menyimpan komentar baru
 Route::post('product/{id}/komentar/{komentarId}/reply', [KomentarController::class, 'storeReply']); // Menyimpan balasan komentar
+
+// rute untuk login register page
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
